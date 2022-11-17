8fa608d821c524a38b329ec258c80d081311fd9b
gwumex <42099116+gwumex@users.noreply.github.com>
2022-11-17 22:19:56 +0100

    add custom validation

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { next } = require('process');
 let bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouther = require('./route/admin/auth');
-
 const app = express();
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(urlencodedParser);
@@ -11,10 +10,13 @@ app.use(cookieSession({
     keys: ['udhdjdjdjdjuiooprtcaieuu89867464je7474y4747446jhsh7474896789e67474902647487494']
 }))
 app.use(authRouther);
-
 app.listen(3000, () => {
     console.log("you are listening on port 3000");
 })
+
+
+
+
     // const bodyParser = (req, res, next) => {
     //     if (req.method === "POST") {
     //         req.on('data', data => {
--- route/admin/auth.js
+++ route/admin/auth.js
@@ -1,31 +1,54 @@
 const usersRepo = require('../../repositories/users');
-const express = require('express'); 
-const signUpTemplate = require('../../views/admin/auth/signup')
-const signInTemplate = require('../../views/admin/auth/signin')
+const express = require('express');
+const signUpTemplate = require('../../views/admin/auth/signup');
+const signInTemplate = require('../../views/admin/auth/signin');
+
+//validation
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
-router.get('/signup',  (req, res) => {
-    res.send(signUpTemplate({req}));
+router.get('/signup', (req, res) => {
+    res.send(signUpTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-    const {email, password, passwordConfirmation} = req.body;
-    const existingUser = await usersRepo.getOneBy({email})
-    if(existingUser){
-        return res.send("user already exist")
-    }
-    if(password !== passwordConfirmation){
-        return res.send("password must match")
-    }
-    ///create a user in our user repo to represent
-    const user = await usersRepo.create({ email: email, password: password});
-
-    req.session.userId = user.id;
-    res.send(`
-    <h1>Sign in complete</h1>
+router.post('/signup',
+    [
+        check('email')
+            .trim()
+            .normalizeEmail()
+            .isEmail()
+            .custom( async (email) => {
+                const existingUser = await usersRepo.getOneBy({ email })
+                if (existingUser) {
+                    throw new Error('email in use');
+                }
+            }),
+        check('password')
+            .trim()
+            .isLength({min: 4, max: 20}),
+        check('passwordConfirmation')
+            .trim()
+            .isLength({min: 4, max: 20})
+            .custom((passwordConfirmation, { req }) => {
+                if (req.body.password !== passwordConfirmation) {
+                    throw new Error('Password must match')
+                }
+            }),
+    ],
+     async (req, res) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        const { email, password, passwordConfirmation } = req.body;
+        ///create a user in our user repo to represent
+        const user = await usersRepo.create({ email: email, password: password });
+        //store the id of that user inside the users cookies
+        req.session.userId = user.id;
+        res.send(`
+    <h1>Signup complete</h1>
     welcome: ${req.session.userId}
     `)
-});
+    });
+
 
 router.get('/signout', (req, res) => {
     req.session = null;
@@ -36,16 +59,16 @@ router.get('/signout', (req, res) => {
 router.get('/signin', (req, res) => {
     res.send(signInTemplate())
 })
-router.post('/signin', async (req, res) =>{
-    const {email, password} = req.body;
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
 
-    const user = await usersRepo.getOneBy({email});
+    const user = await usersRepo.getOneBy({ email });
     if (!user) {
         return res.send('email not found');
     }
-    const compare =  await usersRepo.comparePasswords(user.password, password);
+    const compare = await usersRepo.comparePasswords(user.password, password);
 
-    if(!compare){
+    if (!compare) {
         return res.send('password incorrect')
     }
     res.send('login succesful')
